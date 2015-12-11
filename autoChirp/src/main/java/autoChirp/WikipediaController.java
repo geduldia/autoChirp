c95b68b6aa2b57a92173b4edba41b8fd04d167ba
geduldia <alena_tabea@hotmail.com>
2015-12-11 17:17:33 +0100

    Controller

--- autoChirp/src/main/java/autoChirp/WikipediaController.java
+++ autoChirp/src/main/java/autoChirp/WikipediaController.java
@@ -1,12 +1,12 @@
 package autoChirp;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class WikipediaController {
 
     @RequestMapping(value="/", method=RequestMethod.GET, params="url")
@@ -15,5 +15,4 @@ public class WikipediaController {
        Document doc = parser.parse(url);
        return "title: "+doc.getTitle()+ "\n"+ "lang: "+doc.getLanguage();
     }
-
 }
