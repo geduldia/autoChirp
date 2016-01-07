9456cd7c71983a0cae41db2f55334c0ce215802f
geduldia <alena_tabea@hotmail.com>
2016-01-07 17:24:30 +0100

    format origDate toTweetDate 2

--- autoChirp/src/main/java/autoChirp/TweetFactory.java
+++ autoChirp/src/main/java/autoChirp/TweetFactory.java
@@ -1,16 +1,17 @@
 package autoChirp;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.apache.commons.lang3.time.DateUtils;
 
 import de.unihd.dbs.heideltime.standalone.DocumentType;
 import de.unihd.dbs.heideltime.standalone.OutputType;
@@ -20,21 +21,28 @@ import de.unihd.dbs.heideltime.standalone.exceptions.DocumentCreationTimeMissing
 /**
  * @author geduldia
  * 
- * A class to generate date-related tweets from a document using Heideltime as temporal Tagger
+ *         A class to generate date-related tweets from a document using
+ *         Heideltime as temporal Tagger
  *
  */
 
 public class TweetFactory {
 
+	int currentYear;
+
+	public TweetFactory() {
+		currentYear = LocalDateTime.now().getYear();
+	}
 
 	/**
-	 * @param document  
-	 * @return tweetsByDate - a map of the detected dates and their including sentences trimmed to a tweet-length of 140 characters
-	 * @throws  
+	 * @param document
+	 * @return tweetsByDate - a map of the detected dates and their including
+	 *         sentences trimmed to a tweet-length of 140 characters @throws
 	 */
-	public Map<String, List<String>> getTweets(Document document)  {
+	public Map<String, List<String>> getTweets(Document document) {
 		Map<String, List<String>> tweetsByDate = new TreeMap<String, List<String>>();
-		HeidelTimeWrapper ht = new HeidelTimeWrapper(document.getLanguage(), DocumentType.NARRATIVES, OutputType.TIMEML, "/heideltime/config.props", POSTagger.TREETAGGER, false);
+		HeidelTimeWrapper ht = new HeidelTimeWrapper(document.getLanguage(), DocumentType.NARRATIVES, OutputType.TIMEML,
+				"/heideltime/config.props", POSTagger.TREETAGGER, false);
 		String toProcess = concatSentences(document.getSentences());
 		String processed;
 		try {
@@ -46,19 +54,24 @@ public class TweetFactory {
 		String[] sentences = processed.split("#SENTENCE#");
 		for (int i = 0; i < sentences.length; i++) {
 			String sentence = sentences[i];
+			System.out.println("sentence: " + sentence);
 			List<String> origDates = getDates(sentence);
+			for (String string : origDates) {
+				System.out.println(string);
+			}
 			for (String date : origDates) {
-				List<String> sentenceList = tweetsByDate.get(date);
-				if (sentenceList == null)
+				String tweetDate = getTweetDate(date);
+				List<String> sentenceList = tweetsByDate.get(tweetDate);
+				if (sentenceList == null){
 					sentenceList = new ArrayList<String>();
-			//	sentenceList.add(trimToTweet(document.getSentences().get(i - 1)));
+				}		
 				sentenceList.add(date + ": " + document.getSentences().get(i - 1));
-				tweetsByDate.put(getTweetDate(date), sentenceList);
+			    tweetsByDate.put(tweetDate, sentenceList);
 			}
 		}
+		currentYear = LocalDateTime.now().getYear();
 		return tweetsByDate;
 	}
-
 
 	private String concatSentences(List<String> sentences) {
 		StringBuffer sb = new StringBuffer();
@@ -71,15 +84,15 @@ public class TweetFactory {
 	private String trimToTweet(String sentence) {
 		if (sentence.length() > 140) {
 			sentence = sentence.substring(0, 138);
-			sentence = sentence.substring(0, sentence.lastIndexOf(" "))+"...";
+			sentence = sentence.substring(0, sentence.lastIndexOf(" ")) + "...";
 		}
 		return sentence;
 	}
 
 	private List<String> getDates(String processed) {
 		List<String> dates = new ArrayList<String>();
-		String dateRegex = "<TIMEX3 tid=\"t[0-9]*\" type=\"DATE\" value=\"([0-9|XXXX]{4}-[0-9]{2}(-[0-9]{2})?)\">";
-		String timeRegex = "<TIMEX3 tid=\"t[0-9]*\" type=\"TIME\" value=\"(.{2,50})\">";
+		String dateRegex = "type=\"DATE\" value=\"([0-9|XXXX]{4}-[0-9]{2}(-[0-9]{2})?)\">";
+		String timeRegex = "type=\"TIME\" value=\"(([0-9]{4}|XXXX)-[0-9]{2}(-[0-9]{2})?)(( [A-Z]{2,4})|(T[0-9]{2}:[0-9]{2}(:[0-9]{2})?))\">";
 		Pattern pattern = Pattern.compile(dateRegex);
 		Matcher matcher = pattern.matcher(processed);
 		Pattern pattern2 = Pattern.compile(timeRegex);
@@ -88,30 +101,52 @@ public class TweetFactory {
 		String time = null;
 		while (matcher.find()) {
 			date = matcher.group(1);
-			//only date with at least a month
+			// only date with at least a month
 			if (date.contains("-")) {
 				dates.add(date);
 			}
 			while (matcher2.find()) {
-				time = matcher2.group(1).replace("T", " ");
+				time = matcher2.group(1) + matcher2.group(6).replace("T", " ");
 				dates.add(time);
 			}
 		}
 		return dates;
 	}
-	
 
-	public String getTweetDate(String origDate){
-    	String[] acceptedFormats = {"yyyy-MM-dd","yyyy-MM","yyyy-MM-dd HH:mm", "yyyy-MM-dd HH:mm:ss"};
-    	Date date = null;
-		try {
-			date = DateUtils.parseDate(origDate, acceptedFormats);
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			System.out.println("unable to parse date: " + origDate);
-			return "1111-11-11 11:11:11";
+	public String getTweetDate(String origDate) {
+		boolean midnight = false;
+		if (origDate.contains(" 00:00")) {
+			midnight = true;
 		}
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return formatter.format(date);
+		LocalDateTime ldtOriginal;
+		try{
+			DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+			ldtOriginal = LocalDateTime.parse(origDate, dtFormatter);
+			
+		}
+		catch (DateTimeParseException e){
+			try {
+				LocalDate ldOriginal = LocalDate.parse(origDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+				ldtOriginal = LocalDateTime.of(ldOriginal, LocalTime.of(12, 0));
+			} catch (Exception e2) {
+				LocalDate ldOriginal = LocalDate.parse(origDate.concat("-01"), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+				ldtOriginal = LocalDateTime.of(ldOriginal, LocalTime.of(12, 0));
+			}
+		
+		}
+		//LocalDateTime ldtOriginal = LocalDateTime.parse(origDate, dtfb.toFormatter());
+		LocalDateTime ldt = LocalDateTime.of(currentYear, ldtOriginal.getMonth(), ldtOriginal.getDayOfMonth(),
+				ldtOriginal.getHour(), ldtOriginal.getMinute());
+		LocalDateTime today = LocalDateTime.now();
+		if (ldt.isBefore(today)) {
+			ldt = LocalDateTime.of(currentYear + 1, ldt.getMonth(), ldt.getDayOfMonth(), ldt.getHour(),
+					ldt.getMinute());
+		}
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		String formattedDate = ldt.format(formatter);
+		if (!midnight) {
+			formattedDate = formattedDate.replace(" 00:00", " 12:00");
+		}
+		return formattedDate;
 	}
 }
