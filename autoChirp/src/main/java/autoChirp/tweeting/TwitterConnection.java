8a18ff95c419bea327888f24bc42fec45fa06453
geduldia <ageduldi@uni-koeln.de>
2016-03-08 15:06:55 +0100

    more javaDoc

--- autoChirp/src/main/java/autoChirp/tweeting/TwitterConnection.java
+++ autoChirp/src/main/java/autoChirp/tweeting/TwitterConnection.java
@@ -1,6 +1,5 @@
 package autoChirp.tweeting;
 
-
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -11,44 +10,65 @@ import org.springframework.stereotype.Component;
 import autoChirp.DBConnector;
 import autoChirp.tweetCreation.Tweet;
 
+/**
+ * @author Alena Geduldig
+ * 
+ *         This class executes the actual twitter status-update using
+ *         org.springframework.social.twitter
+ *
+ */
 @Component
 public class TwitterConnection {
 
-@Value("${spring.social.twitter.appId}")
-private String appIDProp;
+	@Value("${spring.social.twitter.appId}")
+	private String appIDProp;
 
-@Value("${spring.social.twitter.appSecret}")
-private String appSecretProp;
+	@Value("${spring.social.twitter.appSecret}")
+	private String appSecretProp;
 
-private static String appID;
-private static String appSecret;
+	private static String appID;
+	private static String appSecret;
 
-@PostConstruct
-public void initializeConnection() {
-        this.appID = appIDProp;
-        this.appSecret = appSecretProp;
-}
+	/**
+	 * get appToken and appSecret
+	 */
+	@PostConstruct
+	public void initializeConnection() {
+		this.appID = appIDProp;
+		this.appSecret = appSecretProp;
+	}
 
-public void run(int user_id, int tweetID) {
-        Tweet toTweet = DBConnector.getTweetByID(tweetID, user_id);
-        if(toTweet == null) {
-                return;
-        }
-        if(toTweet.tweeted) {
-                return;
-        }
-        //check if tweetGroup is still enabled
-        if(!DBConnector.isEnabledGroup(toTweet.groupID, user_id)) {
-                return;
-        }
-
-        String[] userConfig = DBConnector.getUserConfig(user_id);
-        String token = userConfig[1];
-        String tokenSecret = userConfig[2];
-
-        Twitter twitter = new TwitterTemplate(appID, appSecret, token, tokenSecret);
-        twitter.timelineOperations().updateStatus(toTweet.content);
-        DBConnector.flagAsTweeted(tweetID, user_id);
-}
+	/**
+	 * updates the users twitter-status to the tweets content - reads the tweet
+	 * with the given tweetID from the database - checks if the related
+	 * tweetGroup is still enabled and tweet wasn't tweeted already - reads the
+	 * users oAuthToken and oAuthTokenSecret from the database - updates the
+	 * users twitter status to the tweets tweetContent
+	 * 
+	 * @param user_id
+	 * @param tweetID
+	 */
+	public void run(int user_id, int tweetID) {
+		Tweet toTweet = DBConnector.getTweetByID(tweetID, user_id);
+		if (toTweet == null) {
+			return;
+		}
+		//check if tweet was not tweeted already
+		if (toTweet.tweeted) {
+			return;
+		}
+		// check if tweetGroup is still enabled
+		if (!DBConnector.isEnabledGroup(toTweet.groupID, user_id)) {
+			return;
+		}
+		//read userConfig
+		String[] userConfig = DBConnector.getUserConfig(user_id);
+		String token = userConfig[1];
+		String tokenSecret = userConfig[2];
+		//tweeting with org.springframework.social.twitter
+		Twitter twitter = new TwitterTemplate(appID, appSecret, token, tokenSecret);
+		twitter.timelineOperations().updateStatus(toTweet.content);
+		DBConnector.flagAsTweeted(tweetID, user_id);
+	}
 
 }
--- autoChirp/src/main/java/autoChirp/tweeting/TwitterTask.java
+++ autoChirp/src/main/java/autoChirp/tweeting/TwitterTask.java
@@ -1,31 +1,37 @@
 package autoChirp.tweeting;
 
-import java.util.List;
-import java.util.Map;
 import java.util.TimerTask;
 
-import org.springframework.social.twitter.api.Twitter;
-import org.springframework.social.twitter.api.impl.TwitterTemplate;
-
-import autoChirp.DBConnector;
-import autoChirp.tweetCreation.Tweet;
-
-
-
+/**
+ * @author Alena Geduldig
+ * 
+ *         A subclass of java.util.TimerTask related to a single tweet and
+ *         twitter-user. 
+ *         - scheduled by a TweetScheduler timed by the tweets tweetDate
+ *         - the overwritten run()- Method creates a new TwitterConnection which executes the 
+ *         	twitter status-update. 
+ * 
+ */
 public class TwitterTask extends TimerTask {
 
 	private int tweetID;
 	private int user_id;
 
+	/**
+	 * @param user_id
+	 * @param tweetID
+	 */
 	public TwitterTask(int user_id, int tweetID) {
 		this.user_id = user_id;
 		this.tweetID = tweetID;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.util.TimerTask#run()
+	 */
 	@Override
 	public void run() {
 		TwitterConnection tc = new TwitterConnection();
-		System.out.println("tc.run...");
 		tc.run(user_id, tweetID);
 	}
 
