3184ec13a8b093c7a8704bbaa31d0fb2849b0ac0
geduldia <ageduldi@uni-koeln.de>
2016-03-11 15:09:39 +0100

    updated testClasses added CSV-TestFile

--- autoChirp/src/test/java/autoChirp/DBInputOutput.java
+++ autoChirp/src/test/java/autoChirp/DBInputOutput.java
@@ -1,6 +1,5 @@
 package autoChirp;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
--- autoChirp/src/test/java/autoChirp/TweetCreationWorkflow.java
+++ autoChirp/src/test/java/autoChirp/TweetCreationWorkflow.java
@@ -1,71 +1,96 @@
-//
-//package autoChirp;
-//
-//import java.io.File;
-//import java.io.IOException;
-//import java.sql.SQLException;
-//import java.util.List;
-//import java.util.Map;
-//
-//import org.junit.Assert;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//
-//import autoChirp.preProcessing.parser.Parser;
-//import autoChirp.preProcessing.parser.WikipediaParser;
-//import autoChirp.tweetCreation.Tweet;
-//import autoChirp.tweetCreation.TweetFactory;
-//import autoChirp.tweetCreation.TweetGroup;
-//
-///**
-// * @author geduldia
-// *
-// */
-///**
-// * @author geduldia
-// *
-// */
-//public class TweetCreationWorkflow {
-//	
-//	/**
-//	 * @author Alena
-//	 * 
-//	 * junit test-class for the data-mining workflow
-//	 * - insert and read urls to parse
-//	 * - parse urls and extract dates and tweets
-//	 * - store tweets and tweet-date
-//	 */
-//
-//	private static String dbPath = "src/test/resources/";
-//	private static String dbFileName = "autoChirp.db";
-//	private static String dbCreationFileName = "src/main/resources/database/createDatabaseFile.sql";
-//	private static Parser wikiParser = new WikipediaParser();
-//	private static TweetFactory tweetFactory = new TweetFactory();
-//
-//	
-//	@BeforeClass
-//	public static void dbConnection() {
-//		DBConnector.connect(dbPath + dbFileName);
-//		DBConnector.createOutputTables(dbCreationFileName);
-//	}
-//
-//	/**
-//	 * @throws SQLException
-//	 * @throws IOException
-//	 */
-//	@Test
-//	public void buildTweetsFromUrlTest() throws SQLException, IOException {
-//		int userID = DBConnector.insertNewUser(123, "token", "secret");
-//		TweetGroup group = tweetFactory.getTweetsFromUrl("https://de.wikipedia.org/wiki/Star_Wars", wikiParser);
-//		int sizeBefore = group.tweets.size();
-//		Assert.assertTrue(sizeBefore > 0);
-//		int groupID = DBConnector.insertTweetGroup(group, userID);
-//		group = DBConnector.getTweetGroupForUser(userID, groupID);
-//		Assert.assertEquals(sizeBefore, group.tweets.size());
-//		for (Tweet tweet : group.tweets) {
-//			System.out.println("TweetDate: " + tweet.tweetDate);
-//			System.out.println(tweet.content);
-//			System.out.println();
-//		}
-//	}
-//}
+package autoChirp;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import autoChirp.preProcessing.parser.WikipediaParser;
+import autoChirp.tweetCreation.Tweet;
+import autoChirp.tweetCreation.TweetFactory;
+import autoChirp.tweetCreation.TweetGroup;
+
+
+
+/**
+ * @author Alena Geduldig
+ * 
+ * JUnit test-class for the Data-Minint- and TweetCreationWorkflow
+ *
+ */
+public class TweetCreationWorkflow {
+	
+	private static String dbPath = "src/test/resources/";
+	private static String dbFileName = "autoChirp.db";
+	private static String dbCreationFileName = "src/main/resources/database/createDatabaseFile.sql";
+	
+
+	/**
+	 * connect to database and  create output-tables database
+	 */
+	@BeforeClass
+	public static void dbConnection() {
+		DBConnector.connect(dbPath + dbFileName);
+		DBConnector.createOutputTables(dbCreationFileName);
+	}
+
+
+	
+	/**
+	 * test for the tweet creation from wiki-urls
+	 */
+	@Test
+	public void generateTweetsFromWikipediaUrl(){
+		TweetFactory factory = new TweetFactory();
+		List<String> urls = new ArrayList<String>();
+		String url = "https://de.wikipedia.org/wiki/Star_Wars";
+		urls.add(url);
+		url = "https://en.wikipedia.org/wiki/Woody_Allen";
+		urls.add(url);
+		for (String currentUrl : urls) {
+			TweetGroup group = factory.getTweetsFromUrl(currentUrl, new WikipediaParser(),"testDescription", "prefix");
+			System.out.println("Title: " + group.title);
+			System.out.println("Description: " + group.description);
+			System.out.println("numberOfTweets: " + group.tweets.size());
+			System.out.println("Tweets: ");
+			for (Tweet tweet : group.tweets) {
+				Assert.assertTrue(tweet.content.startsWith("prefix: "));
+				System.out.println(tweet.tweetDate+": "+ tweet.content);
+			}
+		}
+	}
+	
+	/**
+	 * test for csv-imports
+	 */
+	@Test
+	public void generateTweetsFromCSVFile(){
+		TweetFactory factory = new TweetFactory();
+		File testFile = new File("src/test/resources/testCSVFile.csv");
+		
+		//without delay
+		TweetGroup group = factory.getTweetsFromExcelFile(testFile, "testTitle", "testDescription", 0);
+		Assert.assertEquals(group.tweets.size(), 5);
+		System.out.println("Title: " + group.title);
+		System.out.println("Description: " + group.description);
+		System.out.println("numberOfTweets: " + group.tweets.size());
+		System.out.println("Tweets: ");
+		for (Tweet tweet : group.tweets) {
+			Assert.assertTrue(tweet.tweetDate.startsWith("2016"));
+			System.out.println(tweet.tweetDate+": "+ tweet.content);
+		}
+		//with delay
+		group = factory.getTweetsFromExcelFile(testFile, "testTitle", "testDescription", 3);
+		Assert.assertEquals(group.tweets.size(), 5);
+		System.out.println("Title: " + group.title);
+		System.out.println("Description: " + group.description);
+		System.out.println("numberOfTweets: " + group.tweets.size());
+		System.out.println("Tweets: ");
+		for (Tweet tweet : group.tweets) {
+			Assert.assertTrue(tweet.tweetDate.startsWith("2019"));
+			System.out.println(tweet.tweetDate+": "+ tweet.content);
+		}
+	}
+}
