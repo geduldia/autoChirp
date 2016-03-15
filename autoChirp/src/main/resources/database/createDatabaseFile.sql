1e20ba7c9fef3c658cb8d7f3f946f084c05c0c0e
geduldia <ageduldi@uni-koeln.de>
2016-03-15 22:01:26 +0100

    deleted unnecessary tables from db

--- autoChirp/src/main/resources/database/createDatabaseFile.sql
+++ autoChirp/src/main/resources/database/createDatabaseFile.sql
@@ -1,10 +1,3 @@
-DROP TABLE IF EXISTS configuration;
-CREATE TABLE configuration (
-  twitter_callback_url VARCHAR(255) NOT NULL,
-  twitter_consumer_key VARCHAR(64) NOT NULL,
-  twitter_consumer_secret VARCHAR(64) NOT NULL
-);
-
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -36,12 +29,4 @@ CREATE TABLE tweets (
   longitude DOUBLE DEFAULT NULL,
   latitude DOUBLE DEFAULT NULL,
   CONSTRAINT FK_USERS_USERID_TWEETS_USERID FOREIGN KEY (user_id) REFERENCES users (user_id)
-);
-
-DROP TABLE IF EXISTS wikipedia;
-CREATE TABLE wikipedia (
-  user_id INTEGER NOT NULL,
-  url VARCHAR(255) NOT NULL,
-  UNIQUE (user_id, url),
-  CONSTRAINT FK_USERS_USERID_WIKIPEDIA_USERID FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
