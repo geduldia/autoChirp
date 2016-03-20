004ee83288f83bd9e62f8448557658fb60ddbef8
geduldia <ageduldi@uni-koeln.de>
2016-03-20 16:17:24 +0100

    handle single quotes in tweetcontent, title or description

--- autoChirp/src/main/java/autoChirp/DBConnector.java
+++ autoChirp/src/main/java/autoChirp/DBConnector.java
@@ -605,10 +605,14 @@ public class DBConnector {
 	public static void editGroup(int groupID, String title, String description, int userID) {
 		try {
 			connection.setAutoCommit(false);
-			Statement stmt = connection.createStatement();
-			String sql = "UPDATE groups SET group_name = '" + title + "', description = '" + description
-					+ "'  WHERE (group_id = '" + groupID + "' AND user_id = '" + userID + "')";
-			stmt.executeUpdate(sql);
+			PreparedStatement stmt = connection.prepareStatement("UPDATE groups SET group_name = ?, description = ?  WHERE (group_id = ?  AND user_id = ?)");
+//			String sql = "UPDATE groups SET group_name = '" + title + "', description = '" + description
+//					+ "'  WHERE (group_id = '" + groupID + "' AND user_id = '" + userID + "')";
+			stmt.setString(1,title);
+			stmt.setString(2, description);
+			stmt.setInt(3, groupID);
+			stmt.setInt(4, userID);
+			stmt.executeUpdate();
 			stmt.close();
 			connection.commit();
 		} catch (SQLException e) {
@@ -638,11 +642,17 @@ public class DBConnector {
 			float latitude) {
 		try {
 			connection.setAutoCommit(false);
-			Statement stmt = connection.createStatement();
-			String sql = "UPDATE tweets SET tweet = '" + content + "', img_url = '" + imageUrl + "', longitude = '"
-					+ longitude + "', latitude = '" + latitude + "'  WHERE (tweet_id = '" + tweetID
-					+ "' AND user_id = '" + userID + "')";
-			stmt.executeUpdate(sql);
+			PreparedStatement stmt = connection.prepareStatement("UPDATE tweets SET tweet = ?, img_url = ?, longitude = ?, latitude = ? WHERE (tweet_id = ? AND user_id = ?)");
+//			String sql = "UPDATE tweets SET tweet = '" + content + "', img_url = '" + imageUrl + "', longitude = '"
+//					+ longitude + "', latitude = '" + latitude + "'  WHERE (tweet_id = '" + tweetID
+//					+ "' AND user_id = '" + userID + "')";
+			stmt.setString(1, content);
+			stmt.setString(2, imageUrl);
+			stmt.setFloat(3, longitude);
+			stmt.setFloat(4, latitude);
+			stmt.setInt(5, tweetID);
+			stmt.setInt(6, userID);
+			stmt.executeUpdate();
 			stmt.close();
 			connection.commit();
 		} catch (SQLException e) {
@@ -665,25 +675,36 @@ public class DBConnector {
 	public static int addTweetToGroup(int userID, Tweet tweet, int groupID) {
 		try {
 			connection.setAutoCommit(false);
-			Statement stmt = connection.createStatement();
-			String sql = "INSERT INTO tweets (user_id, group_id, scheduled_date, tweet, scheduled, tweeted, img_url, longitude, latitude) VALUES ('"
-					+ userID + "', " + "'" + groupID + "', " + "'" + tweet.tweetDate + "', " + "'" + tweet.content
-					+ "', " + "'false', 'false', '" + tweet.imageUrl + "', '" + tweet.longitude + "', '"
-					+ tweet.latitude + "' )";
-			stmt.executeUpdate(sql);
-			stmt.close();
+			//Statement stmt = connection.createStatement();
+			PreparedStatement prepStmt = connection.prepareStatement("INSERT INTO tweets (user_id, group_id, scheduled_date, tweet, scheduled, tweeted, img_url, longitude, latitude) VALUES(?,?,?,?,?,?,?,?,?)");
+//			String sql = "INSERT INTO tweets (user_id, group_id, scheduled_date, tweet, scheduled, tweeted, img_url, longitude, latitude) VALUES ('"
+//					+ userID + "', " + "'" + groupID + "', " + "'" + tweet.tweetDate + "', " + "'" + tweet.content
+//					+ "', " + "'false', 'false', '" + tweet.imageUrl + "', '" + tweet.longitude + "', '"
+//					+ tweet.latitude + "' )";
+			prepStmt.setInt(1, userID);
+			prepStmt.setInt(2, groupID);
+			prepStmt.setString(3, tweet.tweetDate);
+			prepStmt.setString(4, tweet.content);
+			prepStmt.setBoolean(5, false);
+			prepStmt.setBoolean(6, false);
+			prepStmt.setString(7,tweet.imageUrl);
+			prepStmt.setFloat(8, tweet.longitude);
+			prepStmt.setFloat(9, tweet.latitude);
+			prepStmt.executeUpdate();
+			prepStmt.close();
 			connection.commit();
-			stmt = connection.createStatement();
-			sql = "SELECT last_insert_rowid();";
+			Statement stmt = connection.createStatement();
+			String sql = "SELECT last_insert_rowid();";
 			ResultSet result = stmt.executeQuery(sql);
 			int toReturn = result.getInt(1);
 			stmt.close();
+			prepStmt.close();
 			connection.commit();
 			DBConnector.updateGroupStatus(groupID, false, userID);
 			return toReturn;
 
 		} catch (SQLException e) {
-			System.out.print("DBConnector.editTweet: ");
+			System.out.print("DBConnector.addTweetToGroup: ");
 			e.printStackTrace();
 			return -1;
 		}
