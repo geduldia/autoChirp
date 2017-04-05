e78b0b6f4c8ddd378da9c3e5c81d59cfd5746617
Philip Schildkamp <philip.schildkamp@smail.uni-koeln.de>
2017-04-05 13:19:05 +0200

    Add JS char counter for Tweet content

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,3 +2,18 @@ $('body').scrollspy({
     target: '.bs-docs-sidebar',
     offset: 40
 });
+
+if ($('textarea#content').length > 0) {
+	var regex = new RegExp('https?://[^\\s]*', 'g');
+	var subst = new Array(24).join('.');
+
+	$('label[for="content"]').append('<div class="small text-muted">(<span id="charcount">0</span> of 140 chars)</div>');
+
+	$('textarea#content').on('input propertychange', function() {
+		var content = $(this).val().replace(regex, subst);
+		var length = $(this).val().length - content.length;
+
+		$(this).val($(this).val().substring(0, 140 + length));
+		$('span#charcount').html($(this).val().replace(regex, subst).length);
+	});
+}
